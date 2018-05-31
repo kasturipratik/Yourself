ab956f208c018258e9acc65c53b5345610fd4722
kasturipratik <pratik.parajuli@yahoo.com>
2018-05-31 14:40:24 -0400

    Frist Assignment of intellij

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!\n" +
+                "Today is July 19, 2015. I am alive!\n" +
+                "My name is Alton.");
+    }
+}
